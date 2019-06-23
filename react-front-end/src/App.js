8c29d1e0188fa237681ad283768ba84cb6acd75e
Peter <psynevskyy@gmail.com>
2019-06-23 20:36:44 +0000

    added dependencies

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,6 +3,8 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import axios from 'axios';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search-bar';
 import VideoDetail from './components/video-detail';
 const API_KEY = 'AIzaSyBV7rOg-k0NIr7__-LRO_Wz7WVGbF3Dmfw';
@@ -12,8 +14,21 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Click the button to load data!'
+      message: 'Click the button to load data!',
+      videos: [],
+      selectedVideo: null
     }
+
+    this.videoSearch('react js');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
   }
 
   fetchData = () => {
@@ -30,6 +45,10 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+    
     return (
       <div className="App">
                 
