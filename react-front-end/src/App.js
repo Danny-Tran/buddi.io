ec86e789955bb4ca6d078171d4f1bb5986ec7740
Gaganpandher52 <gaganpandher52@gmail.com>
2019-06-24 19:19:41 +0000

    added

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Webcam height={200} />
+          <Webcam height={200}width={200}/>
       </div>
     );
   }
