93004b3d82b1cad98554e8f1a3f368a3fb56d55b
Danny-Tran <truong_tran@live.com>
2019-07-02 20:17:55 +0000

    fixing merging error

--- react-front-end/src/FullRender.js
+++ react-front-end/src/FullRender.js
@@ -51,31 +51,6 @@ class FullRender extends Component {
     }
   }
 
-  popup = (mylink, windowname) => {
-  if (! window.focus)
-    return true;
-  var href;
-  if (typeof(mylink) == 'string')
-    href=mylink;
-  else 
-    href=mylink.href;
-  window.open(href, windowname, 'width=400,height=200,scrollbars=yes');
-  return false;
-}
-
-  // fetchData = () => {
-  //   axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-  //   .then((response) => {
-  //     // handle success
-  //     console.log(response.data) // The entire response from the Rails API
-
-  //     console.log(response.data.message) // Just the message
-  //     this.setState({
-  //       message: response.data.message
-  //     });
-  //   }) 
-  // }
-
   render() {
     if (!this.socket) return null;
     return (
